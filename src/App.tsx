78e862a882b856eef3b2922b5a94f2087df6a463
miawithcode <cuttontail@gmail.com>
2024-08-01 18:18:59 +0800

    feat: add basic folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
+import { FolderIcon } from "@heroicons/react/24/solid";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div className="mx-auto max-w-sm p-8">
+      <ul>
+        <li className="my-1.5">
+          <span className="flex items-center gap-1.5">
+            <FolderIcon className="size-6 text-sky-500" />
+            Home
+          </span>
+
+          <ul className="pl-6">
+            <li className="my-1.5">
+              <span className="flex items-center gap-1.5">
+                <FolderIcon className="size-6 text-sky-500" />
+                Movies
+              </span>
+            </li>
+            <li className="my-1.5">
+              <span className="flex items-center gap-1.5">
+                <FolderIcon className="size-6 text-sky-500" />
+                Music
+              </span>
+            </li>
+            <li className="my-1.5">
+              <span className="flex items-center gap-1.5">
+                <FolderIcon className="size-6 text-sky-500" />
+                Pictures
+              </span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default App;
