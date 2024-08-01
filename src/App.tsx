f0353b61e2635e1bb239dc4acdeae071ef1b0ea7
miawithcode <cuttontail@gmail.com>
2024-08-01 18:24:29 +0800

    feat: add string array data driven component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { FolderIcon } from "@heroicons/react/24/solid";
 
+const folders = ["Movies", "Musics", "Pictures", "Documents"];
+
 const App = () => {
   return (
     <div className="mx-auto max-w-sm p-8">
@@ -11,24 +13,14 @@ const App = () => {
           </span>
 
           <ul className="pl-6">
-            <li className="my-1.5">
-              <span className="flex items-center gap-1.5">
-                <FolderIcon className="size-6 text-sky-500" />
-                Movies
-              </span>
-            </li>
-            <li className="my-1.5">
-              <span className="flex items-center gap-1.5">
-                <FolderIcon className="size-6 text-sky-500" />
-                Music
-              </span>
-            </li>
-            <li className="my-1.5">
-              <span className="flex items-center gap-1.5">
-                <FolderIcon className="size-6 text-sky-500" />
-                Pictures
-              </span>
-            </li>
+            {folders.map((folder) => (
+              <li className="my-1.5" key={folder}>
+                <span className="flex items-center gap-1.5">
+                  <FolderIcon className="size-6 text-sky-500" />
+                  {folder}
+                </span>
+              </li>
+            ))}
           </ul>
         </li>
       </ul>
