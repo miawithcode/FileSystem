80b76c4447f16fafe84d053c35fe5cf8294d7dbc
miawithcode <cuttontail@gmail.com>
2024-08-01 18:33:23 +0800

    feat: add basic subfolder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { FolderIcon } from "@heroicons/react/24/solid";
 
-const folders = ["Movies", "Musics", "Pictures", "Documents"];
+const folders = [
+  { label: "Movies", folders: [{ label: "Action" }, { label: "Comedy" }] },
+  { label: "Musics", folders: [{ label: "Rock" }, { label: "Classical" }] },
+  { label: "Pictures" },
+  { label: "Documents" },
+];
 
 const App = () => {
   return (
@@ -14,11 +19,22 @@ const App = () => {
 
           <ul className="pl-6">
             {folders.map((folder) => (
-              <li className="my-1.5" key={folder}>
+              <li className="my-1.5" key={folder.label}>
                 <span className="flex items-center gap-1.5">
                   <FolderIcon className="size-6 text-sky-500" />
-                  {folder}
+                  {folder.label}
                 </span>
+
+                <ul className="pl-6">
+                  {folder.folders?.map((folder) => (
+                    <li className="my-1.5" key={folder.label}>
+                      <span className="flex items-center gap-1.5">
+                        <FolderIcon className="size-6 text-sky-500" />
+                        {folder.label}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
               </li>
             ))}
           </ul>
